18d93afe3b0732164406f3788258ba9b91cccaa3
Adnan Ozturel <ozturel@google.com>
2020-03-06 14:54:07 +0000

    Add a blank line before module docstrings.

--- src/analyzer/morphotactics/model_compile.py
+++ src/analyzer/morphotactics/model_compile.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Tool to compile morphotactics FST into OpenFST format.
 
 This tool takes a set of lexicon and morphotactics model files and outputs AT&T
